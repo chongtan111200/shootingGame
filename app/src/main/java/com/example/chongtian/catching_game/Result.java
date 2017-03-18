4b26ee02a6098ef7d5c54baf6494b48042d5d388
chongtan111200 <chongtan111200@gmail.com>
2017-03-18 15:15:39 -0400

    First Android shooting game

---
+++ app/src/main/java/com/example/chongtian/catching_game/Result.java
@@ -0,0 +1,49 @@
+package com.example.chongtian.catching_game;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView scoreLabel =(TextView) findViewById(R.id.scoreLabel);
+        TextView highScoreLabel =(TextView) findViewById(R.id.highScoreLabel);
+
+//        int score = getIntent().getIntExtra("SCORE", 0);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int score = extras.getInt("SCORE");
+
+
+        scoreLabel.setText(getString(R.string.score,score));
+
+        SharedPreferences settings = getSharedPreferences("GAME_DATA",MODE_PRIVATE);
+        int highScore = settings.getInt("HIGH_SCORE",0);
+
+        if(score > highScore){
+            highScoreLabel.setText(getString(R.string.highscore,score));
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("HIGH_SCORE",score);
+            editor.apply();
+        }else{
+            highScoreLabel.setText(getString(R.string.highscore,score));
+        }
+        }else{
+            Log.e("Result","no intent found");
+        }
+    }
+
+    public void tryAgain(View view){
+        startActivity(new Intent(getApplicationContext(),Main.class));
+    }
+}
